ad643c15f96d899297412af1949c291173d257a7
otissound <otarshenge@gmail.com>
2023-07-29 19:09:48 +0200

    added test branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -124,6 +124,8 @@
                                 </div>
                             </div>
 
+                            <div><h1>testing</h1></div>
+
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     Coming soon...
